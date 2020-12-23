357e770ae96b69a7b68d19244f60d827f0709634
LeonardoTredese <leonardo.tredese@studenti.unipd.it>
2020-12-23 23:05:29 +0100

    aggiunti controlli input

--- docs/assets/scripts/form.js
+++ docs/assets/scripts/form.js
@@ -1,33 +1,34 @@
-const entryImage = 
+var progressiveId = 0;
+const entryImage =
 					   '<div class="entry">'
-					 +   '<label for="file-immagine-%s" data-msg-required="Per favore, inserisci il file %s">File immagine %s</label>'
-				 	 +	 '<input type="file" id="file-immagine-%s" name="file-immagine-%s"/>'
-				 	 +	 '<label for="alt-immagine-%s" data-msg-required="Per favore, inserisci un testo alternativo per l\'immagine %s">Testo alternativo immagine %s</label>'
+					 +   '<label for="file-immagine-%s" data-error-msg="Il file va inserito, e con estensione .jpg o .png">File immagine</label>'
+				 	 +	 '<input type="file" id="file-immagine-%s" required="required" name="file-immagine-%s"/>'
+				 	 +	 '<label for="alt-immagine-%s" data-error-msg="Il testo alternativo non può essere vuoto o superare i 70 caratteri">Testo alternativo immagine</label>'
 				 	 +	 '<input type="text" id="alt-immagine-%s" name="alt-immagine-%s"'
                  	 +	   'minlength="1" maxlength="70"'
-                 	 +	   'placeholder="Inserisci qui il testo alternativo per l\'immagine %s"'
-                 	 +	   'aria-label="Campo per l\'inserimento per il testo alternativo per l\'immagine %s"/>'
-				 	 +	 '<input type="button" class="remove" value="Rimuovi immagine %s"/>'
+                 	 +	   'placeholder="Inserisci qui il testo alternativo per l\'immagine"'
+                 	 +	   'aria-label="Campo per l\'inserimento per il testo alternativo per l\'immagine"/>'
+				 	 +	 '<input type="button" class="remove" value="Rimuovi immagine"/>'
 					 + '</div>';
 
 
 const entryAttribute = 
 					  '<div class="entry">'
-					+   '<label for="nome-attributo-%s" data-msg-required="Per favore, inserisci un nome per questo attributo" >Nome Attributo %s</label>'
+					+   '<label for="nome-attributo-%s" data-error-msg="Il nome non può essere vuoto o superare i 50 caratteri" >Nome Attributo</label>'
 					+   '<input type="text" id="nome-attributo-%s" name="nome-attributo-%s"'
-                    +  	  'minlength="1"'
-                	+     'placeholder="Inserisci qui il nome dell\'attributo %s"'
+                    +  	  'minlength="1" maxlength="50"'
+                	+     'placeholder="Inserisci qui il nome dell\'attributo"'
 					+	  'required="required"'
                 	+     'aria-required="true"'
-                	+     'aria-label="Campo per nome dell\'attributo %s"/>'
-					+   '<label for="descrizione-attributo-%s" lang data-msg-required="Per favore, inserisci una descrizione per l\'attributo %s" >Descrizione Attributo %s</label>'
-					+   '<textarea id="descrizione-attributo-%s" name="descrizione-attributo-%"'
-                    +     'minlength="1"'
-                	+     'placeholder="Inserisci qui una descrizione per l\'attributo %s"'
+                	+     'aria-label="Campo per nome dell\'attributo"/>'
+					+   '<label for="descrizione-attributo-%s" data-error-msg="La descrizione non può essere vuota o superare i 200 caratteri">Descrizione Attributo</label>'
+					+   '<textarea id="descrizione-attributo-%s" name="descrizione-attributo-%s"'
+                    +     'minlength="1" maxlength="200"'
+                	+     'placeholder="Inserisci qui una descrizione per l\'attributo"'
 					+     'required="required"'
                 	+     'aria-required="true"'
-                	+     'aria-label="Campo per l\'inserimento della descrizione per l\'attributo %s"/></textarea>'
-					+   '<input type="button" id="rimuovi-attributo-%s" class="remove" value="Rimuovi attributo %s"/>'
+                	+     'aria-label="Campo per l\'inserimento della descrizione per l\'attributo"/></textarea>'
+					+   '<input type="button" class="remove" value="Rimuovi attributo"/>'
 					+ '</div>' 
 function ready(callback){
     // in case the document is already rendered
@@ -43,12 +44,49 @@ function ready(callback){
 ready(function(){
 	document.getElementById('invio').onclick = function (event){
 			var t = event.target;
-			console.log(t.id)
 			if (t.className=='remove') t.closest('.entry').remove();
-			else if (t.id=='aggiungi-immagine') addEntry('immagini', entryImage, 0);
-			else if (t.id=='aggiungi-attributo') addEntry('attributi', entryAttribute, 0);
-	} 
+			else if (t.id=='aggiungi-immagine') addEntry('immagini', entryImage, progressiveId++);
+			else if (t.id=='aggiungi-attributo') addEntry('attributi', entryAttribute, progressiveId++);
+	}
+	document.onsubmit = validateForm;
 })
+
+function validateForm (event){
+	deleteErrors();
+	var inputs = document.forms['invio'].querySelectorAll("textarea, input[type=text], input[type=file], input[type=email], input[type=password]");
+	var valid = true;
+	for(var i = 0; i < inputs.length; ++i){
+		if(!validateLength(inputs[i])){
+			showError(inputs[i]);
+			valid = false;
+		}
+	}
+	return valid;
+}
+
+function showError(input){
+	var label = document.querySelector("label[for=%s]".replace(/%s/, input.id));
+	if(label !== null && label.getElementsByClassName("error").length === 0) {
+		var error = document.createElement("strong");
+		error.className = "error";
+		error.appendChild(document.createTextNode(" - " +label.getAttribute("data-error-msg")));
+		label.appendChild(error);
+	}
+		
+}
+
+function deleteErrors(){
+	var errors = document.getElementsByClassName("error");
+	while(0 < errors.length) errors[0].remove();
+}
+
+function validateLength(input){
+	var min = input.getAttribute("minlength");
+	var max = input.getAttribute("maxlength");
+	return ((min === null) || (input.value.length >= min))
+		   && ((max === null) || (input.value.length <= max));
+}
+
 
 function addEntry (framesetId, entryStructure, entryNumber) {
 	var frameset = document.getElementById(framesetId);
