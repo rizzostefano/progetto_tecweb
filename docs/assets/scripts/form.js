fe65448d5e7e2d6d6bbc52f56a6a7e03d207e4a5
Michele Veronesi <micheleveronesi0@gmail.com>
2021-01-10 11:49:28 +0100

    yuhuu

--- docs/assets/scripts/form.js
+++ docs/assets/scripts/form.js
@@ -21,7 +21,9 @@ var regexes = [/(#+)(.*)/,                  // headers
 				/\n(&gt;|\>)(.*)/,          // blockquotea
 				/\n-{5,}/,                  // horiziontal rule
 				/\n([^\n]+)\n/,             // add paragraphs
-				/(\(!)(.+)(\))/]            // image
+				/(\(!)(.+)(\))/,            // image
+				/^(?:\w+,)*\w+$/]			// keywords (comma separated, no blank spaces)
+
 function ready(callback){
     // in case the document is already rendered
     if (document.readyState!='loading') callback();
