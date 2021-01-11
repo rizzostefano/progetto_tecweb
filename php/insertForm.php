4d31050b45077b445f6433a375db03be62298810
Samuele Vanini <samuele.vanini@live.it>
2021-01-11 22:15:12 +0100

    fix bug su display errore nelle keyword, close #47

--- php/insertForm.php
+++ php/insertForm.php
@@ -99,7 +99,7 @@ function validateTextField($field, $minlen, $maxlen, $isNotRequired, $hasMarkdow
 function validateKeywords($keywords) {
 	$errorMessageKeywords = "Le parole chiave sono obbligatorie, devono essere lunghe al massimo 50 caratteri e scritte senza markdown, separate da una virgola e senza spazi";
 	$valid = validateTextField($keywords, NULL, 50, false, false, false) && preg_match("/^(?:\w+,)*\w+$/", $keywords); // regex per il controllo delle keyword inserite
-	handleField($valid, "error-keywords", errorElement($errorMessageKeywords), "%value-keywords", $keywords);
+	handleField($valid, "%error-keywords%", errorElement($errorMessageKeywords), "%value-keywords%", $keywords);
 	return $valid;
 }
 
