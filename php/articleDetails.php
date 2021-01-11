d3e0fc28dbc607823ea93fb151d6297245449ada
Michele Veronesi <micheleveronesi0@gmail.com>
2021-01-11 17:56:40 +0100

    refactoring posizione file

---
+++ php/articleDetails.php
@@ -0,0 +1,51 @@
+<?php
+
+if (!isset($_GET['article_id'])) 
+{
+    header('Location: fallback.php');
+}
+
+require_once('backend/escapeMarkdown.php');
+require_once('backend/article/repoArticle.php');
+require_once('backend/image/repoImage.php');
+
+$repoArticle = new RepoArticle();
+$repoImage = new RepoImage();
+
+$article = $repoArticle->findArticleById($_GET["article_id"]);
+$title = $article->title;
+$title = MarkdownConverter::renderOnlyLanguage($title);
+$content = $article->content;
+$content = MarkdownConverter::render($content);
+
+$articleImage = $repoImage->findImageById($article->image);
+
+$repoArticle->disconnect();
+$repoImage->disconnect();
+
+$DOM = file_get_contents('../visualizzaArticolo.html');
+
+//TODO fare chiamata a db per prendere titolo dell'articolo
+$DOM = str_replace('<cs_page_title/>', $title, $DOM);
+
+//TODO chiedere che meta title inserire
+$DOM = str_replace('<cs_meta_title/>', "<meta name=\"title\" content=\"$title | Rizzo Guitars\"/>", $DOM);
+
+$DOM = str_replace('<cs_meta_description/>', "<meta name=\"description\" content=\"$article->summary\">", $DOM);
+
+$DOM = str_replace('<cs_meta_keyword/>', "<meta name=\"keywords\" content=\"$article->keywords\" />", $DOM);
+
+
+$article = "<section>
+                <h1>$title</h1>
+                $content
+                <div class=\"rectangle-image-cropper-large centered\">
+                    <img src=\"$articleImage->url\" alt=\"$articleImage->alt\" />
+                </div>
+            </section>";
+
+$DOM = str_replace('<cs_main_content/>', $article, $DOM);
+
+echo $DOM;
+
+?>
---
+++ php/backend/image/repoImage.php
@@ -0,0 +1,112 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "dbConnection.php";
+
+class RepoImage{
+
+    private $conn;
+    private $path = "../assets/images/";
+    
+    public function __construct() {
+        $this->conn = new DbConnection();
+    }
+
+    /**
+     * esegue il fetch di tutte le chitarre con tutti i loro dettagli dal db
+     * ritornando un array con oggetti di tipo Guitars
+     */
+    public function getImages()
+    {
+        $query = "SELECT * FROM Images";
+        $stmt = $this->conn->prepareQuery($query);
+        $result = $this->conn->executePreparedQuery($stmt);
+        $images = mysql_fetch_all($result, MYSQLI_ASSOC);
+        $result = array();
+        foreach ($images as $image)
+        {
+            $tmp = new Image($image["Id"], $image["FileName"], $image["Alt"], $image["Url"]);
+            array_push($result, $tmp);
+        }
+        return $result;
+    }
+
+    public function findImageById($imageId)
+    {
+        $query = "SELECT * FROM Images WHERE Id = ?";
+        $stmt = $this->conn->prepareQuery($query);
+        mysqli_stmt_bind_param($stmt, "s", $imageId);
+        $result = $this->conn->executePreparedQuery($stmt);
+        if(mysqli_num_rows($result) === 0) {
+            return false;
+        } else{
+            $result = mysqli_fetch_assoc($result);
+            return new Image($result["Id"], $result["FileName"], $result["Alt"], $result["Url"]);
+        }
+    }
+
+    public function findImageByName($imageName)
+    {
+        $query = "SELECT * FROM Images WHERE FileName = ?";
+        $stmt = $this->conn->prepareQuery($query);
+        mysqli_stmt_bind_param($stmt, "s", $imageName);
+        $result = $this->conn->executePreparedQuery($stmt);
+        if(mysqli_num_rows($result) === 0) {
+            return false;
+        } else{
+            $result = mysqli_fetch_assoc($result);
+            return new Image($result["Id"], $result["FileName"], $result["Alt"], $result["Url"]);
+        }
+    }
+
+    public function addImage($fileImage, $alt)
+    {
+        $filePath = $this->path . $fileImage['name'];
+        $query = "INSERT INTO Images (FileName, Alt, Url) VALUES (?, ?, ?)";
+        $stmt = $this->conn->prepareQuery($query);
+        mysqli_stmt_bind_param($stmt, "sss", $fileImage["name"], $alt, $filePath); 
+        $result = $this->conn->executePreparedQueryDML($stmt);
+        if($result === true) {
+            move_uploaded_file($fileImage["tmp_name"], $filePath);
+            return new Image($this->conn->getInsertID(), $fileImage["name"], $alt, $filePath);
+        } else {
+            return false;
+        }
+    }
+
+    public function editAltImage($id, $alt) {
+        $query = "UPDATE Images SET Alt = ? WHERE Id = ?";
+        $stmt = $this->conn->prepareQuery($query);
+        mysqli_stmt_bind_param($stmt, "ss", $alt, $id);
+        return $this->conn->executePreparedQuery($stmt);
+    }
+
+    public function checkDouble($imageName) {
+        return false !== $this->findImageByName($imageName);
+    }
+
+    public function deleteImage($imageId)
+    {
+        $image = $this->findImageById($imageId);
+        unlink($image->url);
+        $query = "DELETE FROM Images WHERE Id = ?";
+        $stmt = $this->conn->prepareQuery($query);
+        mysqli_stmt_bind_param($stmt, "s", $imageId); 
+        return $this->conn->executePreparedQueryDML($stmt);
+    }
+
+    public function addArticleImage($articleId, $imageId)
+    {
+        $query = "INSERT INTO ArticleImages (IdArticle, IdImage) VALUES (?, ?)";
+        $stmt = $this->conn->prepareQuery($query);
+        mysqli_stmt_bind_param($stmt, "ss", $articleId, $imageId); 
+        return $this->conn->executePreparedQueryDML($stmt);
+    }
+
+    public function disconnect()
+    {
+        $this->conn->disconnect();
+    }
+
+}
+
+?>
