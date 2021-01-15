2921903d717c77363807ddeb2007012cd2b9de0d
LeonardoTredese <leonardo.tredese@studenti.unipd.it>
2021-01-15 12:47:56 +0100

    Aggiunti i comenti dei metodi

--- php/dbConnection.php
+++ php/dbConnection.php
@@ -11,7 +11,9 @@ class DbConnection
     private const DATABASE_NAME = 'test';
     private $current_connection;
 
-    /* Il costruttore della classe tenta di effettuare una connessione al database. In caso di mancata riuscita ritorna a video un errore (non di php) */
+    /**
+	 * @summary Il costruttore della classe tenta di effettuare una connessione al database. In caso di mancata riuscita ritorna a video un errore (non di php)
+     */
     public function __construct()
     {
         if (!($this->current_connection = mysqli_connect(static::HOST, static::USERNAME, static::PASSWORD, static::DATABASE_NAME))) {
@@ -20,34 +22,56 @@ class DbConnection
         }
     }
     
-    /* Il metodo ritorna la corrente connessione */
+    /**
+     * @return il metodo ritorna la connesione corrente di $this
+	 */
     public function getCurrent()
     {
         return $this->current_connection;
     }
 
+	/**
+	 * @param query con zero o più parametri espressi da '?'
+	 * @return sql statement che contiene la $query pronta a ricevere i parametri 
+     */
     public function prepareQuery($query) {
         $stmt = mysqli_stmt_init($this->current_connection);
         mysqli_stmt_prepare($stmt, $query);
         return $stmt;
     }
 
+	/**
+	 * @param stmt statement con una query di interrogazione dati
+	 *        a cui sono già stati forniti gli  eventuali parametri
+	 * @return il risultato di $stmt appilcato a $this->current_connection
+	 */
     public function executePreparedQuery($stmt) // select
     {
         mysqli_stmt_execute($stmt);
         return mysqli_stmt_get_result($stmt);
     }
 
+	/**
+	 * @param stmt statement con una query di manipulazione  dati
+	 *        a cui sono già stati forniti gli  eventuali parametri
+	 * @return il risultato di $stmt appilcato a $this->current_connection
+	 */
     public function executePreparedQueryDML($stmt) // insert e update
     {
         return mysqli_stmt_execute($stmt);
     }
 
+	/**
+	 * @return L'id generato dall'ultima query di manipolazione dati
+	 *         riuscita su $this->current_connection
+ 	 */
     public function getInsertId(){
         return  mysqli_insert_id($this->current_connection);
     }
 
-    /* Il metodo esegue la disconnessione dal database */
+    /**
+	 * @summary viene eseguita la disconnessione di $this->current_connection 
+	 */
     public function disconnect()
     {
         mysqli_close($this->current_connection);
